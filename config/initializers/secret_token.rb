7d8b4df15773f7728493fd2230727be12313ecd1
Darcy Murphy <mrDarcyMurphy@gmail.com>
2010-09-28 21:14:04 -0500

    Rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeresaMurphy::Application.config.secret_token = 'b8a1056c02f7352f2dbd321b968e7520a18eaf6f6d5d501a6c637403ecda3d5c4e1995d36c959c0af4d3a6bc75de99145f72982722c8f4b614516ac5a0eecfe1'
